149f2b6aa186193ddca22e142a7f487d3e729650
Colette-Lo <clo11@srrcc.og.uk>
2025-01-07 09:10:20 +0000

    The window for the game is made.

---
+++ screens.py
@@ -0,0 +1,14 @@
+# Using the library tkinter to create the user interfaces.
+import tkinter as tk
+
+
+# Create the window for the game.
+window = tk.Tk()
+
+# Provide the resolution and title of the window.
+window.geometry("1920x1820")
+window.title("Country Simulation")
+
+
+
+window.mainloop()
