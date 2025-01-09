9c888a2c4abeb0869d673d1234ad6563d8ba24dd
Colette-Lo <clo11@srrcc.og.uk>
2025-01-09 21:09:43 +0000

    Scenario info box screen: layout finished. ready to implement text.

---
+++ scenario_info_screen.py
@@ -0,0 +1,36 @@
+import tkinter as tk
+
+# Scenario screen where the player choose a policy
+# Create the window
+window = tk.Tk()
+
+# Provide the resolution and title of the window.
+window.geometry("700x500")
+window.title("Information Box")
+
+# Heading
+page_heading = tk.Label(window,
+                        text="Context",
+                        font=('Arial', 20),
+                        bg="white",
+                        width=20,
+                        height=1
+                        )
+page_heading.pack(padx=10, pady=30)
+
+# Content
+description = tk.Label(window,
+                       text="Description",
+                       font=('Arial', 20),
+                       bg="white",
+                       height=10,
+                       width=35)
+description.pack(pady=10)
+
+
+window.mainloop()
+
+
+
+
+
