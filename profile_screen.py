891ff3bc00ad740bfe8f3904aead5a94a9f96ea8
Colette-Lo <clo11@srrcc.org.uk>
2025-02-28 12:28:31 +0000

    tested indicators methods

--- profile_screen.py
+++ profile_screen.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import PhotoImage
-from PIL import Image, ImageTk
+# from PIL import Image, ImageTk
 from screens import Screen
 
 # # Country profile
@@ -62,7 +62,7 @@ from screens import Screen
 # window.mainloop()
 
 class ProfileScreen(Screen):
-    def __init__(self, name, flag_path,  population_size):
+    def __init__(self, name, flag_path, population_size):
         super().__init__()
         self.heading = tk.Label(self,
                                 text="Profile",
@@ -146,10 +146,14 @@ class ProfileScreen(Screen):
             self.level = 3
 
     def cal_gdp(self, scale):
-        # store the current value
-        original = self.values[5]
-        # calculate the new value
-        new = original * (1+scale)
+        # the limit for GDP is >0
+        if scale >= -1:
+            # store the current value
+            original = self.values[5]
+            # calculate the new value
+            new = original * (1+scale)
+        else:
+            new = 0
         # update the value to be display as the GDP
         self.values[5] = new
         # a print statement for testing
@@ -190,12 +194,12 @@ class ProfileScreen(Screen):
             # calculate the new value
             new = original * (1+scale)
             # checking if the new value is still within the range
-            if 0 <= new <= 100:
+            if 0 <= new <= 10:
                 self.values[2] = new
             elif new < 0:
                 self.values[2] = 0
             else:
-                self.values[2] = 100
+                self.values[2] = 10
         # a print statement for testing
         print("Happiness index:", self.values[2])
 
@@ -205,7 +209,7 @@ class ProfileScreen(Screen):
             # store the current value
             original = self.values[1]
             # calculate the new value
-            new = original * (1+scale)
+            new = original + scale
             # checking if the new value is still within the range
             if 0 <= new <= 100:
                 self.values[1] = new
@@ -223,9 +227,10 @@ class ProfileScreen(Screen):
             original = self.values[0]
             # calculate the new value
             new = original * (1+scale)
+            rounded_new = round(new, 1)
             # checking if the new value is still within the range
-            if 0 <= new:
-                self.values[0] = new
+            if 0 < new:
+                self.values[0] = rounded_new
             else:
                 self.values[0] = 0.5
         # a print statement for testing
@@ -233,12 +238,5 @@ class ProfileScreen(Screen):
 
 # , [12, 123, 1234, 123, 1234, 1234]
 tryscreen = ProfileScreen("nhk", "C:/Users/colet/OneDrive/Desktop/flag_1.png", "12345")
-
-tryscreen.cal_gdp(0.3)
-tryscreen.cal_unemployment(0.3)
-tryscreen.cal_inflation(0.3)
-tryscreen.cal_happiness(0.3)
-tryscreen.cal_literacy(0.3)
-tryscreen.cal_life_expectancy(0.3)
 
 # tryscreen.mainloop()
